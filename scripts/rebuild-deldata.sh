d50e71b09328cf9fad21cd3b3cdf9eef851bed32
Jason mbikayi <jason.mbikayi@gmail.com>
2025-09-18 09:57:13 -0400

    updated rebuild-deldata.sh scripts

--- scripts/rebuild-deldata.sh
+++ scripts/rebuild-deldata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 docker-compose down
 docker volume rm p2p-payments_db_data
+docker-compose build --no-cache
+docker-compose up -d
 ./restore-db.sh
-docker-compose build --no-cache && docker-compose up -d
 docker-compose build --no-cache backend
 docker-compose build --no-cache frontend
 docker ps -a
