5cc0adea4f34484d0c01dca1ec41ab17794c7e70
jasonmbikayi <100853893+jasonmbikayi@users.noreply.github.com>
2025-09-19 13:09:33 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-     https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../ssl/node-selfsigned.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../ssl/node-selfsigned.pem')),
-    },   
+  //   https: {
+  //   key: fs.readFileSync(path.resolve(__dirname, '../ssl/node-selfsigned.key')),
+  //   cert: fs.readFileSync(path.resolve(__dirname, '../ssl/node-selfsigned.pem')),
+  // },   
     host: '0.0.0.0',
     port: 5173,
     proxy: {
